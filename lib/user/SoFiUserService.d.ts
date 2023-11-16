2ac002045abd48b507795f527880c9a3bacafc83
yjgaia <hanul@hanul.me>
2023-11-16 15:54:41 +0900

    dev

--- lib/user/SoFiUserService.d.ts
+++ lib/user/SoFiUserService.d.ts
@@ -1,5 +1,6 @@
 import { UserService } from "common-app-module";
 import SoFiUserPublic from "../database-interface/SoFiUserPublic.js";
 export default class SoFiUserService<T extends SoFiUserPublic> extends UserService<T> {
+    fetchByXUsername(xUsername: string): Promise<T | undefined>;
 }
 //# sourceMappingURL=SoFiUserService.d.ts.map
--- src/user/SoFiUserService.ts
+++ src/user/SoFiUserService.ts
@@ -3,4 +3,10 @@ import SoFiUserPublic from "../database-interface/SoFiUserPublic.js";
 
 export default class SoFiUserService<T extends SoFiUserPublic>
   extends UserService<T> {
+  public async fetchByXUsername(xUsername: string): Promise<T | undefined> {
+    const data = await this.safeFetch((b) =>
+      b.select(this.selectQuery).eq("x_username", xUsername)
+    );
+    return data?.[0];
+  }
 }
