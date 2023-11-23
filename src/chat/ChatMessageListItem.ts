bbfd0cee708c95059debe0c16132dc0f5e60efbe
yjgaia <hanul@hanul.me>
2023-11-23 20:46:18 +0900

    dev

--- src/chat/ChatMessageListItem.ts
+++ src/chat/ChatMessageListItem.ts
@@ -12,34 +12,49 @@ export default class ChatMessageListItem extends SoFiComponent {
     super(".chat-message-list-item");
     const message = messages[0];
     if (message) {
-      this.append(
-        el(".author-profile-image", {
-          style: {
-            backgroundImage: `url(${message.author.profile_image_thumbnail})`,
-          },
+      const authorProfileImage = el(".author-profile-image", {
+        style: {
+          backgroundImage: `url(${message.author.profile_image_thumbnail})`,
+        },
+        click: (event) => this.goAuthorProfile(event),
+      });
+
+      const authorInfoDisplay = el(
+        ".author",
+        el(".name", message.author.display_name, {
           click: (event) => this.goAuthorProfile(event),
         }),
-        el(
-          "main",
-          el(
-            "header",
-            el(
-              ".author",
-              el(".name", message.author.display_name, {
-                click: (event) => this.goAuthorProfile(event),
-              }),
-            ),
-            el(".date", DateUtil.fromNow(message.created_at)),
-          ),
-          ...messages.map((message) =>
-            new ChatMessageDisplay(message, {
-              owner: options.signedUserId !== undefined &&
-                message.author.user_id === options.signedUserId,
-              new: options.newMessageIds.includes(message.id),
-            }, interactions)
-          ),
-        ),
       );
+
+      const dateDisplay = el(".date", DateUtil.fromNow(message.created_at));
+
+      const messageDisplays = messages.map((message) =>
+        new ChatMessageDisplay(message, {
+          owner: options.signedUserId !== undefined &&
+            message.author.user_id === options.signedUserId,
+          new: options.newMessageIds.includes(message.id),
+        }, interactions)
+      );
+
+      if (message.author.user_id === options.signedUserId) {
+        this.append(
+          el(
+            "main",
+            el("header", dateDisplay, authorInfoDisplay),
+            ...messageDisplays,
+          ),
+          authorProfileImage,
+        ).addClass("authored");
+      } else {
+        this.append(
+          authorProfileImage,
+          el(
+            "main",
+            el("header", authorInfoDisplay, dateDisplay),
+            ...messageDisplays,
+          ),
+        );
+      }
     }
   }
 
