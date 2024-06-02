320e1f1d688c6d6fe75cbcbd34b488817d04e6c7
yjgaia <yjdev88@gmail.com>
2024-06-02 16:54:01 +0900

    Add primary button to LeaveFeedbackModal

--- lib/feedback/LeaveFeedbackModal.d.ts
+++ lib/feedback/LeaveFeedbackModal.d.ts
@@ -1,7 +1,6 @@
 import { AdaptiveModal } from "@common-module/app";
 export default class LeaveFeedbackModal extends AdaptiveModal {
     private input;
-    private submitButton;
     constructor(signedUserId: string | undefined);
 }
 //# sourceMappingURL=LeaveFeedbackModal.d.ts.map
